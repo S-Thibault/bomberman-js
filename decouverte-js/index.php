56eb016f36b1f8e387334b6cc6a97c9de36998be
S-Thibault <sylvain.thibault@infamail.com>
2020-06-17 11:01:49 +0200

    Add files via upload

---
+++ decouverte-js/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./style.css">
+    <title>Plateau-js</title>
+  </head>
+  <body>
+    <div class="plateau">
+    </div>
+  </body>
+</html>
