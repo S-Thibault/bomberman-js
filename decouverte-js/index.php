e457fa219900854c41326729588992809df746b1
S-Thibault <sylvain.thibault@infamail.com>
2020-06-17 11:07:03 +0200

    Update index.php

--- decouverte-js/index.php
+++ decouverte-js/index.php
@@ -3,10 +3,14 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="./style.css">
+    <script src="script.js" charset="utf-8"></script>
     <title>Plateau-js</title>
   </head>
   <body>
     <div class="plateau">
+    </div>
+
+    <div class="pion">
     </div>
   </body>
 </html>
