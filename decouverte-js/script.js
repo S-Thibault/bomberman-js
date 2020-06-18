53840618e27741c7d49089a1a29277f2ede2350c
S-Thibault <sylvain.thibault@infamail.com>
2020-06-18 17:01:38 +0200

    Update script.js

--- decouverte-js/script.js
+++ decouverte-js/script.js
@@ -17,7 +17,7 @@ document.onkeydown = function(event){
   var event = event || window.event,
   keyCode = event.keyCode;
 
- switch(keyCode){
+  switch(keyCode){
     case 38:
       y = y - GRID_SIZE;
       if (y < 0){
@@ -47,11 +47,11 @@ document.onkeydown = function(event){
     break;
   }
 
+
   s.left = String(x) + 'px';
   s.top = String(y) + 'px';
   console.log(x , y);
 }
-
 
 var blockGrid = [];
 for(var i = 0; i < H_GRID; i++){
@@ -62,8 +62,14 @@ for(var i = 0; i < H_GRID; i++){
   block.style.height = "40px";
   block.style.display = "flex";
   block.style.position = "absolute";
-  if (random100() > 90) block.style.backgroundColor = "black";
-  else block.style.backgroundColor = "green";
+
+  if (random100() > 90){} block.style.backgroundColor = "black";
+  block.traverser = false;
+  }
+
+  else { block.style.backgroundColor = "green";
+    block.traverser = true;
+}
   block.style.marginLeft = (i * GRID_SIZE).toString()+"px";
   block.style.marginTop = (j * GRID_SIZE).toString()+"px";
 
@@ -72,8 +78,47 @@ for(var i = 0; i < H_GRID; i++){
   }
 }
 
-blockGrid[10][10].style.backgroundColor = "blue";
+//blockGrid[10][10].style.backgroundColor = "blue";
 
+document.onkeydown = function(event){
+  var event = event || window.event,
+  keyCode = event.keyCode;
+
+  switch(keyCode){
+    case 38:
+      y = y - GRID_SIZE;
+      if (y < 0){
+      y = 0
+    }
+    break;
+
+    case 39:
+      x = x + GRID_SIZE;
+      if (x > WINDOW_WIDTH - GRID_SIZE && blockGrid[y / GRIDE_SIZE][x / GRIDE_SIZE]){
+      x = WINDOW_WIDTH - GRID_SIZE
+    }
+    break;
+
+    case 40:
+      y = y + GRID_SIZE;
+      if (y > WINDOW_HEIGHT - GRID_SIZE){
+      y = WINDOW_HEIGHT - GRID_SIZE
+    }
+    break;
+
+    case 37:
+      x = x - GRID_SIZE;
+      if (x < 0){
+      x = 0
+    }
+    break;
+  }
+
+
+  s.left = String(x) + 'px';
+  s.top = String(y) + 'px';
+  console.log(x , y);
+}
 
 function randomColor(){
 return "#" + ((1<<24)*Math.random()|0).toString(16);
