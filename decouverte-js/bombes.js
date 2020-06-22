b6eb6ff762c15314803f684d63a84626f416f3cc
S-Thibault <sylvain.thibault@infamail.com>
2020-06-22 16:55:43 +0200

    Add files via upload

---
+++ decouverte-js/bombes.js
@@ -0,0 +1,7 @@
+document.onkeydown = function(event){
+  var event = event || window.event,
+  keyCode = event.keyCode;
+
+
+case 32: // touche "Espace"
+  let bombe = = document.createElement("div");
