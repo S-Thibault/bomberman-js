d66a50976aef8ea2d7dee0b5170dcc3ae3635b96
S-Thibault <sylvain.thibault@infamail.com>
2020-06-22 15:34:13 +0200

    Update index.php

--- decouverte-js/index.php
+++ decouverte-js/index.php
@@ -7,6 +7,17 @@
   </head>
   <body>
 
+    <div id="general">
+        <ul id="commandes">
+          <img src="img/EVAbackground" alt="">
+          <h3> Commandes </h3>
+          <li> &#x21E7; : Haut</li>
+          <li> &#x21E9; : Bas</li>
+          <li> &#x21E6; : Gauche</li>
+          <li> &#x21E8; : Droite</li>
+          <li> Espace : Bombe</li>
+        </ul>
+
   <div id="plateau">
     <div id="personnage">
     </div>
