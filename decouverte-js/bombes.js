31599130dfafb5d453fd7c3f0d2e132a997959b0
S-Thibault <sylvain.thibault@infamail.com>
2020-06-24 16:47:09 +0200

    Update bombes.js

--- decouverte-js/bombes.js
+++ decouverte-js/bombes.js
@@ -1,7 +1,1 @@
-document.onkeydown = function(event){
-  var event = event || window.event,
-  keyCode = event.keyCode;
 
-
-case 32: // touche "Espace"
-  let bombe = = document.createElement("div");
